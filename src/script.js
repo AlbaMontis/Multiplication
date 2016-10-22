c4d21c0c13da024cd6270633204b9db9b8b187a7
John Smith <john@example.com>
2016-10-22 16:14:26 +0300

    Code refactoring

--- src/script.js
+++ src/script.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
 	var firstNum = $('#firstNum').text(Math.floor((Math.random() * 10) + 1));
 	var secondNum = $('#secondNum').text(Math.floor((Math.random() * 10) + 1));
-	var mult = +firstNum.text() * +secondNum.text();
-	var coins = localStorage.getItem('sum') || 0;
-	var ans;
+	var mult = parseInt(firstNum.text()) * parseInt(secondNum.text());
+	var coins = parseInt(localStorage.getItem('sum')) || 0;
+	var solution;
 
   $('#coin').text(coins);
 
-	$('#submit').click(function() {
-		ans = $('#solution').val();
-		if (mult == ans) {
+	$('#submit').click(function(event) {
+		event.preventDefault();
+		solution = $('#solution').val();
+    
+		if (mult == solution) {
 			$('#feedback').text('Вірно! Молодець!');
-			$('#coin').text(coins=+coins+10);
-			localStorage.setItem('sum', coins);
+			$('#coin').text(coins+5);
+			localStorage.setItem('sum', coins+5);
+
 		} else {
-			$('#feedback').text('Не вірно. Спробуй ще раз.');
+			$('#feedback').text('Неправильно. Спробуй ще раз.');
 		}
 	});
 
